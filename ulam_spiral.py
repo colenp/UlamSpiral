898e7e594c89ca41f60560535d491ad448111e7b
Cole Normandin-Parker <114674686+colenp@users.noreply.github.com>
2022-11-26 20:23:31 -0600

    terminal only works for 5, 25, etc

---
+++ ulam_spiral.py
@@ -0,0 +1,53 @@
+import turtle
+import math
+#controls size of display
+NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0) # directions
+turn_right = {NORTH: E, E: S, S: W, W: NORTH} # old -> new direction
+
+
+def is_prime(n): # (O(n^(1.2))) for faster calculation of prime
+  for i in range(2,int(math.sqrt(n))+1):
+    if (n%i) == 0:
+      return False
+  return True
+
+
+def spiral(width, height):
+    if width < 1 or height < 1:
+        raise ValueError
+    x, y = width // 2, height // 2 # start near the center
+    dx, dy = NORTH # initial direction
+    matrix = [[None] * width for _ in range(height)]
+    count = 0
+    while True:
+        count += 1
+        matrix[y][x] = count # visit
+        # try to turn right
+        new_dx, new_dy = turn_right[dx,dy]
+        new_x, new_y = x + new_dx, y + new_dy
+        if (0 <= new_x < width and 0 <= new_y < height and
+            matrix[new_y][new_x] is None): # can turn right
+            x, y = new_x, new_y
+            dx, dy = new_dx, new_dy
+        else: # try to move straight
+            x, y = x + dx, y + dy
+            if not (0 <= x < width and 0 <= y < height):
+                return matrix # nowhere to go
+
+def print_matrix(matrix):
+    width = len(str(max(el for row in matrix for el in row if el is not None)))
+    fmt = "{:0%dd}" % width
+    for row in matrix:
+        print(" ".join("_"*width if el is None else fmt.format(el) for el in row))
+    return spiral
+
+if __name__ == "__main__":
+    spiral_size = 625
+    m = 25
+    lst = []
+    matrix = spiral(m,m)
+    for i in range(m):
+        for j in range(m):
+            if not is_prime(matrix[i][j]):
+                matrix[i][j] = 0
+    print_matrix(matrix)
